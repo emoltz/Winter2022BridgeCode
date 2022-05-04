51ef80658d1683469c7ea020bc38a58e6dbaeb01
emoltz <es5541@nyu.edu>
2022-05-04 15:57:01 -0400

    prettyPrint red/black trees

--- May4.cpp
+++ May4.cpp
@@ -4,6 +4,27 @@ using namespace std;
 
 //how would you print the minimum value in a queue?
 
+//for printing red black trees
+template<class T>
+void RedBlackNode<T>::prettyPrint(int indent) const {
+    if (right != nullptr) {
+        right->prettyPrint(indent + 1);
+    }
+    int margin = indent * 2;
+    for (int i = 0; i < margin; ++i) {
+        cout << '\t';
+    }
+    cout << "DATA: " << data << endl;
+    for (int i = 0; i < margin; ++i) {
+        cout << '\t';
+    }
+    cout << "COLOR: " << (color == RED ? "RED" : "BLACK") << endl;
+    if (left != nullptr) {
+        left->prettyPrint(indent + 1);
+    }
+}
+
+
 int main(){
     //
 
