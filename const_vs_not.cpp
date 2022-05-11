4a32ba4a336b972917872d63c63e099f8eb60675
emoltz <es5541@nyu.edu>
2022-05-11 16:51:08 -0400

    const

---
+++ const_vs_not.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+const int DONOTCHANGE = 5;
+
+void move (const int &var);
+void move (int var);
+
+class Thing{
+public:
+    int move (int var) const;
+};
+
+
+
+
+int main(){
+    vector<int> v;
+
+
+}
