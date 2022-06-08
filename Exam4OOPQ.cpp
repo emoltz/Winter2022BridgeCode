8eea35da89c1855e1eef3a2e9a14f116c9a6419b
emoltz <es5541@nyu.edu>
2022-06-08 16:55:30 -0400

    with challenge project!

---
+++ Exam4OOPQ.cpp
@@ -0,0 +1,11 @@
+//A music streaming service would like us to create classes for use in their system. We need to track:
+/*
+
+* users
+* songs
+* advertisements
+* release date
+* users can earn points by listening to songs, so we need to track each person's "points"
+* users can "unlock" songs with their points, so we need to track how many points a song has, and then deduct those points from the user
+
+*/ 
